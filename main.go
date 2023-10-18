eda0a2cc329085544c0ab3892363e3e03d4f2870
Otonashi Kaguya <tsukiianya@gmail.com>
2023-10-18 18:07:32 -0400

    Added sudoer and adding new items

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ import (
 type Config struct {
 	Token  string `json:"token"`
 	Prefix string `json:"prefix"`
+	Sudoer string `json:"sudoer"`
 	DbProt string `json:"dbProt"`
 	DbUser string `json:"dbUser"`
 	DbPass string `json:"dbPass"`
@@ -43,8 +44,8 @@ type Item struct {
 	Id   int
 	Name string
 	Desc string
-	Cost string
-	Sell string
+	Cost int
+	Sell int
 }
 
 type Store struct {
@@ -113,6 +114,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 
 	log.Println(m.Content)
+	log.Println(m.Author.ID)
 
 	// Handle bot commands.
 	if strings.HasPrefix(m.Content, config.Prefix) {
@@ -148,23 +150,77 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 			}
 		}
 
-		// Handle 'newplayer' command.
-		if strings.HasPrefix(command, "newplayer") {
-			// Extract userID and name from the message.
-			parts := strings.Split(command, " ")
-			if len(parts) != 3 {
-				log.Println("Invalid command format. Correct usage: !newplayer <@userID> Name")
-				return
+		//Sudoer Commands
+		if m.Author.ID == config.Sudoer {
+			// Handle 'newplayer' command.
+			if strings.HasPrefix(command, "newplayer") {
+				// Extract userID and name from the message.
+				parts := strings.Split(command, " ")
+				if len(parts) != 3 {
+					log.Println("Invalid command format. Correct usage: !newplayer <@userID> Name")
+					return
+				}
+
+				userID := parts[1]
+				name := parts[2]
+
+				// Call newPlayer to create a new player in the database.
+				err := newPlayer(userID, name, db, config.DbName)
+				if err != nil {
+					log.Println("Error creating new player:", err.Error())
+					return
+				}
 			}
 
-			userID := parts[1]
-			name := parts[2]
+			// Handle 'newitem' command.
+			if strings.HasPrefix(command, "newitem") {
+				// Example input string format: "<Stick><Just a stick><10><20>"
+				// Remove the command prefix and angle brackets
+				input := strings.TrimPrefix(command, "newitem ")
+				input = strings.Trim(input, "<>")
+				parts := strings.Split(input, "><")
 
-			// Call newPlayer to create a new player in the database.
-			err := newPlayer(userID, name, db, config.DbName)
-			if err != nil {
-				log.Println("Error creating new player:", err.Error())
-				return
+				if len(parts) != 4 {
+					log.Println("Invalid command format. Correct usage: !newitem <name><desc><cost><sell>")
+					return
+				}
+
+				// Extract parameters
+				itemName := parts[0]
+				itemDesc := parts[1]
+
+				// Parse cost and sell values
+				itemCost, err := strconv.Atoi(parts[2])
+				if err != nil {
+					log.Println("Invalid item cost:", err.Error())
+					return
+				}
+
+				itemSell, err := strconv.Atoi(parts[3])
+				if err != nil {
+					log.Println("Invalid item sell value:", err.Error())
+					return
+				}
+
+				// Get the next available item ID.
+				nextItemID, err := getNextItemID(db, config.DbName)
+				if err != nil {
+					log.Println("Error getting next item ID:", err.Error())
+					return
+				}
+
+				// Call newItem to create a new item in the database.
+				err = newItem(nextItemID, itemName, itemDesc, itemCost, itemSell, db, config.DbName)
+				if err != nil {
+					log.Println("Error creating new item:", err.Error())
+					return
+				}
+
+				_, err = s.ChannelMessageSend(m.ChannelID, "New item added successfully.")
+				if err != nil {
+					log.Println("Error sending message:", err.Error())
+					return
+				}
 			}
 		}
 	}
@@ -293,4 +349,67 @@ func newPlayer(userID, name string, client *mongo.Client, dbName string) error {
 	}
 
 	return nil
+}
+
+func updateItem(client *mongo.Client, item *Item, dbName string) error {
+	collection := client.Database(dbName).Collection("BathtubItems")
+
+	filter := bson.M{"id": item.Id}
+	existingItem := Item{}
+	err := collection.FindOne(context.TODO(), filter).Decode(&existingItem)
+
+	if err == nil {
+		update := bson.M{"$set": bson.M{"name": item.Name, "desc": item.Desc, "cost": item.Cost, "sell": item.Sell}}
+		_, err = collection.UpdateOne(context.TODO(), filter, update)
+		if err != nil {
+			return fmt.Errorf("error updating item: %v", err)
+		}
+	} else {
+		// Player doesn't exist, insert as a new player
+		_, err = collection.InsertOne(context.TODO(), item)
+		if err != nil {
+			return fmt.Errorf("error inserting new itemr: %v", err)
+		}
+	}
+	return nil
+}
+
+func newItem(id int, name string, desc string, cost int, sell int, client *mongo.Client, dbName string) error {
+	newItem := Item{
+		Id:   id,
+		Name: name,
+		Desc: desc,
+		Cost: cost,
+		Sell: sell,
+	}
+
+	err := updateItem(client, &newItem, dbName)
+	if err != nil {
+		return fmt.Errorf("error creating new item: %v", err)
+	}
+
+	return nil
+}
+
+func getNextItemID(client *mongo.Client, dbName string) (int, error) {
+	collection := client.Database(dbName).Collection("BathtubItems")
+
+	// Sort the items by ID in descending order and find the maximum.
+	opts := options.Find().SetSort(bson.D{{"id", -1}}).SetLimit(1)
+	cur, err := collection.Find(context.TODO(), bson.D{}, opts)
+	if err != nil {
+		return 0, err
+	}
+	defer cur.Close(context.TODO())
+
+	var item Item
+	if cur.Next(context.TODO()) {
+		if err := cur.Decode(&item); err != nil {
+			return 0, err
+		}
+		return item.Id + 1, nil
+	}
+
+	// If no items found, start with ID 1.
+	return 1, nil
 }
